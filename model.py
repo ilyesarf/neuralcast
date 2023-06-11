d7007a1678e85d0b770e4bf7d6c335546a382eed
Quimzy <bam.boy2020@gmail.com>
2023-06-11 21:02:18 +0300

    train function

--- model.py
+++ model.py
@@ -4,12 +4,10 @@ import torch
 import torch.nn as nn
 from sklearn.model_selection import train_test_split
 
-# Step 1: Preparing the CSV Data
 data = pd.read_csv('data/EEG_Eye_State_Classification.csv')
 features = data.iloc[:, :-1].values
 labels = data.iloc[:, -1].values
 
-# Step 2: Data Preprocessing
 features = np.reshape(features, (features.shape[0], 1, 1, features.shape[1]))
 features = torch.tensor(features, dtype=torch.float32)
 labels = torch.tensor(labels, dtype=torch.long)
@@ -40,11 +38,10 @@ class EEGNet(nn.Module):
         x = x.view(x.size(0), -1)  # Flatten the tensor
         x = self.classify(x)
         return x
-    
-if __name__ == '__main__':
+
+def train():   
     model = EEGNet()
 
-    # Step 5: Model Compilation and Training
     lossfunc = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     epochs = 100
@@ -62,14 +59,16 @@ if __name__ == '__main__':
             optimizer.step()
         print(f"Epoch {epoch+1}/{epochs}, Loss: {loss.item()}")
 
+    return model
+
+if __name__ == '__main__':
+    model = train()
     model.eval()
 
-    # Step 6: Model Evaluation
     with torch.no_grad():
         test_outputs = model(test_features)
     _, predicted_labels = torch.max(test_outputs.data, 1)
     accuracy = (predicted_labels == test_labels).sum().item() / len(test_labels)
     print(f"Accuracy: {accuracy}")
 
-    # Step 7: Model Deployment and Prediction
     torch.save(model.state_dict(), 'eeg_model.pth')
